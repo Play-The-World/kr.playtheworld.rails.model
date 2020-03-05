6336bc3fc9be678e91e0680c1f8ff559aed41877
Eric <spb828@naver.com>
2020-03-05 14:21:20 +0900

    DB 수정

--- db/migrate/20200109072832_create_model_tokens.rb
+++ db/migrate/20200109072832_create_model_tokens.rb
@@ -12,7 +12,7 @@ class CreateModelTokens < ActiveRecord::Migration[6.0]
       t.timestamps
 
       # Indexes
-      t.index [:tokenable_id, :tokenable_type, :type, :expired_at, :value], name: "token_index"
+      t.index [:tokenable_id, :tokenable_type, :type, :value], name: "token_index"
     end
   end
 end
