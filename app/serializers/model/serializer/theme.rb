43fc915b27c619af8f01249a12272e35cf4097ac
Eric <spb828@naver.com>
2020-03-06 18:27:06 +0900

    Update Theme

--- app/serializers/model/serializer/theme.rb
+++ app/serializers/model/serializer/theme.rb
@@ -4,7 +4,15 @@ module Model::Serializer
     set_id :fake_id
 
     # Attributes
-    attributes :start_stage_list_number
+    attributes :start_stage_list_number,
+      :difficulty,
+      :status,
+      :type,
+      :theme_type,
+      :price,
+      :play_time,
+      :data_size,
+      :super_plays_count
     
     # Relations
     has_many :stage_lists
