d4466c9e8842a4e44cc3c6cc49a49f228bc50192
Eric <spb828@naver.com>
2020-08-18 17:20:07 +0900

    냥토리얼 플레이 테스트

--- seeds/test.rb
+++ seeds/test.rb
@@ -1,5 +1,53 @@
-# puts Model::SuperTheme::Base.first.as_json
+def print(play:)
+  puts "현재 스테이지 목록 표시"
+  puts "스테이지 리스트 수: #{play.stage_lists.size}"
+  play.stage_lists.each do |sl|
+    puts "/-----------------------------------/"
+    puts "StageList #{sl.stage_list_number}: #{sl.title}"
+    sl.stages.each do |s|
+      puts " - Stage #{s.id}"
+    end
+  end
+  puts "/-----------------------------------/"
+end
 
-# puts Model::SuperTheme::Base.new.as_json
+def submit(play:, answer:)
+  sl = play.stage_lists.last
+  str = "[Action] #{sl.stage_list_number}. #{sl.title} #{answer} 정답 제출 -> "
+  if play.submit_answer(answer)
+    str += "정답"
+  else
+    str += "오답"
+  end
+  puts str
+  puts ""
+end
 
-puts Model::Play::Base.new.as_json
+user = Model::User::Base.first
+user ||= Model::User::Base.create!(
+  email: "mechuri@playthe.world",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
+# Theme
+# 냥토리얼
+theme = Model::SuperTheme::Base.where(title: "냥토리얼").take.themes.take
+
+# Start Play
+play = user.play_solo(theme: theme)
+
+puts "첫 스테이지들"
+print(play: play)
+
+# 정답 제출
+submit(play: play, answer: "곽두팔")
+print(play: play)
+
+# 오답 제출
+submit(play: play, answer: "123")
+print(play: play)
+
+# 정답 제출
+submit(play: play, answer: "231")
+print(play: play)
