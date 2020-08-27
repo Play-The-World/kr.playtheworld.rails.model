1d622a90c2b742640602a80ec725eabe9dd5d8c4
Eric <spb828@naver.com>
2020-08-27 18:21:47 +0900

    Condition: DateTime

---
+++ app/models/model/condition/after_date.rb
@@ -0,0 +1,35 @@
+module Model::Condition # :nodoc:
+  #
+  # AfterDate 조건 클래스
+  # TODO 조건에 대한 설명
+  #
+  # == Relations
+  #
+  # ==== belongs_to
+  #
+  # * Conditionable(polymorphic)
+  # * Conditioner(polymorphic)
+  #
+  # ==== has_many
+  # 
+  # * ConditionClear
+  class AfterDate < Base
+    # 조건을 만족하는 지 여부
+    # 
+    # ==== Return
+    # 
+    # * Bool
+    def cleared?
+      # TODO 조건 명세
+      (Time.now.to_datetime >= value1.to_datetime)
+    rescue
+      super
+    end
+
+    # 조건을 만족했다는 것을 표시하는 함수
+    # (ex. ConditionClear를 생성)
+    def clear!
+      
+    end
+  end
+end
