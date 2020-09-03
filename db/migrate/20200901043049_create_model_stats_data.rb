fd2f803dddaeafc1eaa2b95847fb22d30602a204
Eric <spb828@naver.com>
2020-09-03 14:08:03 +0900

    fix: StatsData 오타 수정 및 인덱스 네이밍

--- db/migrate/20200901043049_create_model_stats_data.rb
+++ db/migrate/20200901043049_create_model_stats_data.rb
@@ -2,7 +2,7 @@ class CreateModelStatsData < ActiveRecord::Migration[6.0]
   def change
     create_table :model_stats_data do |t|
       # Relations
-      t.references :statsable, polymorhpic: true, index: false
+      t.references :statsable, polymorphic: true, index: false
 
       # Attributes
       t.string :type
@@ -11,7 +11,7 @@ class CreateModelStatsData < ActiveRecord::Migration[6.0]
       t.timestamps
 
       # Indexes
-      t.index [:statsable_id, :statsable_type, :type, :value]
+      t.index [:statsable_id, :statsable_type, :type, :value], name: 'stats_data_index'
     end
   end
 end
