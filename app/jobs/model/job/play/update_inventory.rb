4056fb4f850e56c11ac0d25c646aa770460a2d38
Eric <spb828@naver.com>
2020-09-04 14:19:30 +0900

    job: update_ineventory

--- app/jobs/model/job/play/update_inventory.rb
+++ app/jobs/model/job/play/update_inventory.rb
@@ -6,7 +6,19 @@ module Model::Job::Play
     private
       def job(options)
         # Pusher: Inventory UI Update
-        puts "hello!"
+        items = play.inventory.items.as_json(:play)
+        play.pusher(
+          event: :update_inventory,
+          params: {
+            items: items
+          }
+        )
+        
+        # FOR TEST
+        # a = {}
+        # a[:event] = :update_inventory
+        # a[:params] = { items: items }
+        # puts a
       end
       def debounce?; true end
       def delay; 1 end
